1441d1118bf4cdb740adfd8e4a612abab2e4f481
MarcosF <marcosfelipemaia2001@gmail.com>
2022-07-01 19:29:53 -0300

    update main.js improving timer improving timer

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-let start = document.querySelector("#start");
-let reset = document.querySelector("#reset");
+let start = document.querySelector("#start")
+let reset = document.querySelector("#reset")
 
-let minute = 0;
-let seconds = 0;
-let miliSeconds = 0; 
-let time = 1000
+let finish = document.querySelector("#finish")
+let audio = document.querySelector('#sound')
+
+let counter = document.querySelector("#counter")
+
+let time = 1
+let miliSeconds = 0
+let seconds = 0
+let minute = 0
 let interval;
 
-
     start.addEventListener("click", () => {
-        interval = setInterval(() => {
-            watch();
-        }, time);
-    });
+       interval =  setInterval (() => {
+        watch();
+       }, time);
+
+     
+    })
 
     reset.addEventListener("click", () => {
        clearInterval(interval)
-       minute = 0;
-       seconds = 0;
-       miliSeconds = 0;
-       document.querySelector("#counter").innerText = `00:00:00`;
-    
-    });
+       seconds = 0
+       minute = 0
+       document.querySelector("#counter").innerText = `00:00:00`
+       finish.value = ''
+    })
 
     function watch () {
         miliSeconds++;
@@ -35,17 +40,23 @@ let interval;
                 minute++;
             }
         }
-
+        
         let format = (minute < 10 ? '0' + minute : minute) + ':' + (seconds <10 ? '0' + seconds : seconds) + ':' + (miliSeconds < 10 ? '0' + miliSeconds : miliSeconds);
         document.querySelector("#counter").innerText = format;
 
-
-        if(minute == 10) {
+        if(minute == finish.value) {
             clearInterval(interval)
             minute = 0;
             seconds = 0;
             miliSeconds = 0;
-            document.querySelector("#counter").innerText = "Acabou o Intervalo vamos voltar a aula"
+            audio.play()
         }
-    };
 
+        if(finish.value <= 0) {
+            alert("impossivel cronometra informe um valor para ser terminado")
+            minute = 0;
+            seconds = 0;
+            miliSeconds = 0;
+            audio.pause()
+        }
+    }
